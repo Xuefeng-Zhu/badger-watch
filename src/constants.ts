d136a1027ea14c7f7c97231e847da5cf32553add
Xuefeng Zhu <frankzhu94@gmail.com>
2021-09-08 19:51:13 -0700

    add abtrium support

--- src/constants.ts
+++ src/constants.ts
@@ -21,6 +21,14 @@ export const NETWORKS: any = {
         rpcUrl: 'https://matic-mainnet.chainstacklabs.com',
         explorer: 'https://polygonscan.com',
     },
+    42161: {
+        name: 'Arbitrum',
+        chainId: 42161,
+        badgerRegistry: '0xfda7eb6f8b7a9e9fcfd348042ae675d1d652454f',
+        rpcUrl:
+            'https://arb-mainnet.g.alchemy.com/v2/4SFZ5_A5Ng2gl_xTWF-RrAzFVxOgI4c9',
+        explorer: 'https://arbiscan.io',
+    },
 };
 
 export const getNetworkById: (chainId: number) => NetworkInfo = (
